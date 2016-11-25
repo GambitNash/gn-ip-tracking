f3a28b40891ca00f6121207214ad3d523dd990e0
danw310895 <danw310895@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-11-25 12:08:36 +0000

    Spaces become tabs (in line with wordpress coding standards)
    git-svn-id: https://plugins.svn.wordpress.org/gn-ip-tracking@1540555 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class.gn-ip-tracking-frontend.php
+++ class.gn-ip-tracking-frontend.php
@@ -9,7 +9,7 @@
 
 // Make sure we're in wordpress (not being called directly) and the plugin is loaded.
 if ( !defined( 'ABSPATH' ) || !defined( 'GN_IP_TRACKING_VERSION' ) ) {
-    exit();
+		exit();
 }
 
 /**
@@ -19,50 +19,50 @@ if ( !defined( 'ABSPATH' ) || !defined( 'GN_IP_TRACKING_VERSION' ) ) {
  * enqueuing front-end scripts.
  */
 class GN_IP_Tracking_Frontend {
-  private $ipt_core = null;
+	private $ipt_core = null;
 
-  public function __construct( $ipt_core ) {
-    $this->ipt_core = $ipt_core;
+	public function __construct( $ipt_core ) {
+		$this->ipt_core = $ipt_core;
 
-    // Register Scripts
-    add_action( 'wp_enqueue_scripts', array( $this, 'register_scripts' ) );
+		// Register Scripts
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_scripts' ) );
 
-    // See if we're frontend, configured and active...
-    if ( ! is_admin() && $this->ipt_core->is_configured() !== false ) {
-      // We are active and front-end; Enqueue the registered scripts
-      add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		// See if we're frontend, configured and active...
+		if ( ! is_admin() && $this->ipt_core->is_configured() !== false ) {
+			// We are active and front-end; Enqueue the registered scripts
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 
-      // Also hook the tracking trigger to the 'wp_footer' action - this outputs the tracking ID and trigger.
-      add_action( 'wp_footer', array( $ipt_core, 'output_ip_tracking_footer_scripts' ), 30 );
-    }
-  }
+			// Also hook the tracking trigger to the 'wp_footer' action - this outputs the tracking ID and trigger.
+			add_action( 'wp_footer', array( $ipt_core, 'output_ip_tracking_footer_scripts' ), 30 );
+		}
+	}
 
-  /**
-   * Register GN IP Tracking Scripts.
-   *
-   * Hooked to the 'wp_enqueue_scripts' action.
-   */
-  public function register_scripts() {
-    // pt.js is the external tracker and responsible for tracking the visitors
-    $src = 'https://stats.gambitnash.co.uk/stats/pt.js';
+	/**
+	 * Register GN IP Tracking Scripts.
+	 *
+	 * Hooked to the 'wp_enqueue_scripts' action.
+	 */
+	public function register_scripts() {
+		// pt.js is the external tracker and responsible for tracking the visitors
+		$src = 'https://stats.gambitnash.co.uk/stats/pt.js';
 
-    // Register pt.js for use in the footer of the site with no dependencies.
-    wp_register_script( 'gn-ip-tracking', $src, array(), null, true );
-  }
+		// Register pt.js for use in the footer of the site with no dependencies.
+		wp_register_script( 'gn-ip-tracking', $src, array(), null, true );
+	}
 
-  /**
-   * Enqueue GN IP Tracking Scripts.
-   *
-   * Hooked to the 'wp_enqueue_scripts' action but only when the request is
-   * front-end and the site has IP Tracking enabled and configured.
-   *
-   * The 'gn-ip-tracking' script (pt.js) has already been registered by this
-   * class at this point.
-   *
-   * @see GN_IP_Tracking_Frontend::register_scripts
-   */
-  public function enqueue_scripts() {
-    wp_enqueue_script( 'gn-ip-tracking' );
-  }
+	/**
+	 * Enqueue GN IP Tracking Scripts.
+	 *
+	 * Hooked to the 'wp_enqueue_scripts' action but only when the request is
+	 * front-end and the site has IP Tracking enabled and configured.
+	 *
+	 * The 'gn-ip-tracking' script (pt.js) has already been registered by this
+	 * class at this point.
+	 *
+	 * @see GN_IP_Tracking_Frontend::register_scripts
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_script( 'gn-ip-tracking' );
+	}
 
 }
--- gn-ip-tracking.php
+++ gn-ip-tracking.php
@@ -20,7 +20,7 @@ Text Domain: gn-ip-tracking
 
 // Make sure we're in wordpress (and not being called directly)
 if ( !defined( 'ABSPATH' ) ) {
-    exit();
+		exit();
 }
 
 // Define some constants to be used in other classes.
@@ -30,7 +30,7 @@ define( 'GN_IP_TRACKING_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 // Load the core class GN_IP_Tracking and register the activation hook.
 if ( ! class_exists('GN_IP_Tracking') )
-  require_once( GN_IP_TRACKING_PLUGIN_DIR . 'class.gn-ip-tracking.php' );
+	require_once( GN_IP_TRACKING_PLUGIN_DIR . 'class.gn-ip-tracking.php' );
 
 $ipt_core = new GN_IP_Tracking();
 register_activation_hook( __FILE__ , array( $ipt_core, 'activate_plugin' ) );
@@ -38,15 +38,15 @@ register_activation_hook( __FILE__ , array( $ipt_core, 'activate_plugin' ) );
 
 // Load the front-end class - GN_IP_Tracking_Frontend.
 if ( ! class_exists('GN_IP_Tracking_Frontend') )
-  require_once( GN_IP_TRACKING_PLUGIN_DIR . 'class.gn-ip-tracking-frontend.php' );
+	require_once( GN_IP_TRACKING_PLUGIN_DIR . 'class.gn-ip-tracking-frontend.php' );
 
 new GN_IP_Tracking_Frontend($ipt_core);
 
 
 // Load the admin-only class - GN_IP_Tracking_Admin - but only if is_admin.
 if ( is_admin() ) {
-  if ( ! class_exists('GN_IP_Tracking_Admin') )
-	   require_once( GN_IP_TRACKING_PLUGIN_DIR . 'class.gn-ip-tracking-admin.php' );
+	if ( ! class_exists('GN_IP_Tracking_Admin') )
+		require_once( GN_IP_TRACKING_PLUGIN_DIR . 'class.gn-ip-tracking-admin.php' );
 
-  new GN_IP_Tracking_Admin($ipt_core);
+	new GN_IP_Tracking_Admin($ipt_core);
 }
